a1c35b8025d3445fd52b82825a4bfcb98b3815c1
shengdanxue <email>
2025-01-16 16:16:12 +0800

    营业额统计 2025年1月16日16:16:09

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -4,7 +4,12 @@ package com.sky.mapper;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mapper
 public interface OrderMapper {
     public void insert(Orders orders);
+
+    Double sumByMap(Map map);
 }
